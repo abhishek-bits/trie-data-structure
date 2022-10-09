8cb6349df59519c343069d223ce488d7598361b1
abhishek-bits <h20211030070@hyderabad.bits-pilani.ac.in>
2022-10-09 14:53:23 +0530

    make trie an interface

--- src/trie/Trie.java
+++ src/trie/Trie.java
@@ -1,96 +1,25 @@
 package trie;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Implementation of Trie Data Structure
+ * Trie interface specifying the functionalities that any implementing class should provide.
  * 
  * @author https://github.com/abhishek-bits
+ * 
  */
-public class Trie {
+public interface Trie {
 
-	TrieNode root = null;
-	
-	public Trie() {
-		root = new TrieNode();
-	}
-	
 	/**
 	 * Inserts the given word into the Trie
 	 * @param word
 	 */
-	public void add(String word) {
-		
-		TrieNode curr = root;
-		
-		for(char c : word.toCharArray()) {
-			
-			int idx = c - 'a';
-			
-			// If this character doesn't exist in trie
-			// add it
-			if(curr.children[idx] == null) {
-				curr.children[idx] = new TrieNode();
-			}
-			
-			curr = curr.children[idx];
-		}
-		
-		// this word ends here, so we mark it.
-		curr.isEndingChar = true;
-	}
+	public void add(String word);
 	
 	/**
 	 * Returns a list of words starting from the given prefix
 	 * @param prefix
 	 * @return
 	 */
-	public List<String> getWords(String prefix) {
-		
-		List<String> res = new ArrayList<>();
-		TrieNode par = root;
-		
-		for(char c : prefix.toCharArray()) {
-			int idx = c - 'a';
-			par = par.children[idx];
-		}
-		
-		// first check if this prefix is itself an 
-		if(par.isEndingChar) {
-			res.add(prefix);
-		}
-		
-		// Now, find out other words starting with this prefix
-		populateList(res, par, prefix);
-		
-		return res;
-	}
-	
-	/**
-	 * Recursive method to populate the list of matching words
-	 * from the given prefix string by traversing through the Trie.
-	 * @param res
-	 * @param par
-	 * @param prefix
-	 */
-	private void populateList(List<String> res, TrieNode par, String prefix) {
-		
-		for(int i = 0; i < 26; i++) {
-			
-			TrieNode child = par.children[i];
-			
-			if(child != null) {
-				
-				char c = (char) (i + 'a');
-				
-				populateList(res, child, prefix + c);
-				
-				if(child.isEndingChar) {
-					res.add(prefix + c);
-				}
-			}
-		}
-		
-	}
+	public List<String> getMatchingWords(String prefix);
 }
